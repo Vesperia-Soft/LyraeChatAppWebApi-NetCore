88cdddc7cf9b3762f48bbda896f3c5cf74aebfc7
batuhan topcu <topcubatuhan00@gmail.com>
2023-07-12 15:34:57 +0300

    structure completed

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './layout/dashboard'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className='container-fluid' style={{ background: '#f2ebfb' }}>
+				<Dashboard />
+			</div>
+		</div>
+	);
 }
 
 export default App;
