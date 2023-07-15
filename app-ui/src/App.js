6111350b12f48b3be85605a5006aefdfd3431cbf
batuhan topcu <topcubatuhan00@gmail.com>
2023-07-15 15:22:43 +0300

    signalR pt-1

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -6,16 +6,48 @@ import 'semantic-ui-css/semantic.min.css';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Lobby from './components/Lobbyy';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { useState } from 'react';
+
 
 function App() {
+	// return (
+	// 	<div className="App">
+	// 		<div className='container-fluid' style={{ background: '#f2ebfb' }}>
+	// 			<Dashboard />
+	// 		</div>
+	// 		<ToastContainer theme='dark' />
+	// 	</div>
+	// );
+	const [connection, setConnection] = useState();
+	const joinRoom = async (user, room) => {
+		try {
+			const connection = new HubConnectionBuilder()
+				.withUrl("https://localhost:7246/chat")
+				.configureLogging(LogLevel.Information)
+				.build();
+
+			connection.on("ReceiveMessage", (user, message) => {
+				console.log('message received', message);
+			});
+
+			await connection.start();
+			await connection.invoke("joinRoom", {user, room});
+			setConnection(connection)
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}
+
 	return (
-		<div className="App">
-			<div className='container-fluid' style={{ background: '#f2ebfb' }}>
-				<Dashboard />
-			</div>
-			<ToastContainer theme='dark' />
+		<div className='app'>
+			<h2>MyChat</h2>
+			<hr className='line' />
+			<Lobby joinRoom={joinRoom} />
 		</div>
-	);
+	)
 }
 
 export default App;
