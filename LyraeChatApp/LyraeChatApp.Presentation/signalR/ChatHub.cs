6bc788afb913426f6a0a577e1cd5fad9c3a2c05f
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-07-15 15:29:04 +0300

    signalr backend codes sent. integration continues.

---
+++ LyraeChatApp/LyraeChatApp.Presentation/signalR/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LyraeChatApp.Presentation.signalR;
+
+public class ChatHub :Hub
+{
+    private readonly string _botUser;
+    private readonly IDictionary<string, UserConnection> _connection;
+
+    public ChatHub(IDictionary<string,UserConnection> connections)
+    {
+        _botUser = "MyChat Bot";
+        _connection = connections;
+    }
+
+    //public override Task OnDisconnectedAsync(Exception? exception)
+    //{
+    //    if(_connection.TryGetValue(Context))
+    //}
+    public async Task SendMessage(string message)
+    {
+        if(_connection.TryGetValue(Context.ConnectionId,out UserConnection userConnection))
+        {
+            await Clients.Group(userConnection.Room)
+                .SendAsync("ReceiveMessage", userConnection.User, message);
+        }
+    }
+    public async Task JoinRoom(UserConnection userConnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+        _connection[Context.ConnectionId] = userConnection;
+
+        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,$"{userConnection.User} has joined {userConnection.Room}");
+    }
+}
