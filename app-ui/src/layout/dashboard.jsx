e50b5b01e681c437565c9a01eb369516bf865702
batuhan topcu <topcubatuhan00@gmail.com>
2023-07-27 02:04:32 +0300

    contacts operation completed

--- app-ui/src/layout/dashboard.jsx
+++ app-ui/src/layout/dashboard.jsx
@@ -57,7 +57,7 @@ export default function Dashboard() {
             setConnection(connection)
         }
         catch (e) {
-            console.log(e);
+            // console.log(e);
         }
     }
 
