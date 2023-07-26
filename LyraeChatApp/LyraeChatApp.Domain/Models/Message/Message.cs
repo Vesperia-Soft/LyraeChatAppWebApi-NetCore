e98cc9a5a916170555fa375115599c456ba01c56
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-07-27 00:11:13 +0300

    message creation complete. test successful

--- LyraeChatApp/LyraeChatApp.Domain/Models/Message/Message.cs
+++ LyraeChatApp/LyraeChatApp.Domain/Models/Message/Message.cs
@@ -7,5 +7,4 @@ public class Message : EntityBase
     public string Text { get; set; }
     public int UserId { get; set; }
     public int RoomId { get; set; }
-    public DateTime TimeStamps { get; set; }
 }
